4aaabc1f31d900edc8296a42dbb2ab26afe701fb
Breno Cardoso <elisiobreno@gmail.com>
2023-04-22 15:33:31 -0300

    Refactoring app Teams

--- teams/models.py
+++ teams/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Team(models.Model):
   name = models.CharField(max_length=50)
@@ -7,4 +8,14 @@ class Team(models.Model):
   stadium = models.CharField(max_length=100)
   founded_in = models.IntegerField()
   status = models.IntegerField(default=1)
-  created_at = models.DateTimeField(auto_now_add=True)
+  created_at = models.DateTimeField(default=datetime.now())
+  updated_at = models.DateTimeField(default=datetime.now())
+
+  def save(self, *args, **kwargs):
+    if not self.id:
+      self.created_at = datetime.now()
+    self.updated_at = datetime.now()
+    return super(Team, self).save(*args, **kwargs)
+
+  class Meta:
+    verbose_name_plural = "Teams"
--- teams/views.py
+++ teams/views.py
@@ -1,6 +1,6 @@
 from teams.serializers import TeamSerializer
 from teams.models import Team
-from rest_framework import viewsets
+from rest_framework import mixins, permissions, viewsets
 
 class TeamViewSet(viewsets.ModelViewSet):
   queryset = Team.objects.all()
